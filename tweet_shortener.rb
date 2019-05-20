7f8a777670afec8ccdbcedc6067871413a666b76
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-20 08:17:02 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,3 +33,9 @@ def bulk_tweet_shortener(array)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.size > 140
+    word_substituter(tweet)
+  end
+ende
