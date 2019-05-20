98c3783ac3308a6637f209242621925f8b0b9cb5
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-20 07:54:07 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,7 +16,7 @@ def word_substituter (string)
   keys = dictionary.keys
 
   splat = string.split(" ")
-  splat.collect do |word|
+  splat.each do |word|
     if keys.include?(word)
       word = "hi"
     end
